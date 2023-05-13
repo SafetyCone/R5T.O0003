50e4c40d406541b2daf0ededa9ab177e231a6a4e
davidcoats <david@getsafetycone.com>
2023-05-13 14:17:06 -0700

    Initial commit.

---
+++ source/R5T.O0003.Construction/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.O0003.Construction
+Construction console project for the R5T.O0003 library.
---
+++ source/R5T.O0003/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.O0003
+{
+	/// <summary>
+	/// Common code file generation operations library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
